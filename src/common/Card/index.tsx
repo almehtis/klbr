ed776d594061d36dd52196e3e736bfff38bd8efc
almehtis <esriechtnachwermut@gmail.com>
2023-02-03 18:20:08 +0300

    refactor: changes after review

--- src/common/Card/index.tsx
+++ src/common/Card/index.tsx
@@ -1,17 +1,31 @@
-import { memo } from "react";
+import { LegacyRef, memo } from "react";
+import { useInView } from 'react-intersection-observer'
 import cn from "classnames"
 import "./Card.css"
 
 export const Card = memo(function Card(props: {
   className?: string;
-  key?: number;
+  cardKey: number;
   userName: string;
   userSurname: string;
 }) {
+  const {ref, inView} = useInView({
+    threshold: 1,
+    triggerOnce: true
+  })
+
   return (
-    <div className={cn('card', props.className)} key={props.key}>
-      <span>Name: {props.userName}</span>
-      <span>Surname: {props.userSurname}</span>
-    </div>
+    <li className={cn('card',
+      {
+        'card_inactive': !inView && props.cardKey > 20,
+        'card_active': inView && props.cardKey > 20
+      })}
+
+      key={props.cardKey}
+      ref={ref}
+      >
+        <span>Name: {props.userName}</span>
+        <span>Surname: {props.userSurname}</span>
+    </li>
   );
 });
