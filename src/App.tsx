9112e8b86a61e35a42a2aa5a4b235e7459a05e2c
almehtis <esriechtnachwermut@gmail.com>
2023-02-04 15:33:34 +0300

    refactor: changes after review

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
 import { useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 import { Card } from './common/Card'
 import { User } from './common/types/user'
 import './App.css'
 
 function App() {
-  const [users, setUsers] = useState<{name: string, surname: string}[]>([])
+  const [users, setUsers] = useState<User[]>([])
+  const [visibleItems, setVisibleItems] = useState(21)
+
+  const {ref, inView} = useInView({
+    threshold: 0,
+    triggerOnce: true
+  })
 
   useEffect(() => {
-    fetch('https://api.json-generator.com/templates/8QBvRSsQuyLn/data?access_token=f36pphzkqs7tjpt08asn0tlv36xmbzzaae88fa94')
-    .then(response => response.json())
-    .then(usersList => {setUsers(usersList)})
+      fetch('https://api.json-generator.com/templates/8QBvRSsQuyLn/data?access_token=f36pphzkqs7tjpt08asn0tlv36xmbzzaae88fa94')
+      .then(response => response.json())
+      .then(usersList => {setUsers(usersList)})
   }, [])
+
+  useEffect(() => {
+    if(inView) {
+      setVisibleItems(visibleItems + 20)
+    }
+
+    return
+  }, [inView])
+
 
   return (
     <div className="App">
       <ul className="users-list">
-        {users.slice(0, 101).map((user: User, index: number) =>
-          <Card cardKey={index} userName={user.name} userSurname={user.surname} />
+        {users.slice(0, visibleItems).map((user: User, index: number) =>
+          <Card
+            cardRef={ref}
+            cardKey={index}
+            userName={user.name}
+            userSurname={user.surname}
+          />
         )}
       </ul>
     </div>
--- src/common/Card/index.tsx
+++ src/common/Card/index.tsx
@@ -1,31 +1,32 @@
-import { memo } from "react";
+import { LegacyRef, memo } from 'react'
 import { useInView } from 'react-intersection-observer'
-import cn from "classnames"
-import "./Card.css"
+import cn from 'classnames'
+import './Card.css'
 
 export const Card = memo(function Card(props: {
   className?: string;
   cardKey: number;
+  cardRef: LegacyRef<HTMLDivElement>;
   userName: string;
   userSurname: string;
 }) {
   const {ref, inView} = useInView({
-    threshold: 1,
-    triggerOnce: true
+    threshold: .5,
+    triggerOnce: true,
   })
 
   return (
-    <li className={cn('card card_first-twenty',
-      {
-        'card_inactive': !inView && props.cardKey > 20,
-        'card_active': inView && props.cardKey > 20
-      })}
-
+    <li
+      className="card-wrapper"
       key={props.cardKey}
       ref={ref}
-      >
+    >
+      <div className={cn('card', {'card_inactive': !inView}, props.className)} ref={props.cardRef}>
         <span>Name: {props.userName}</span>
         <span>Surname: {props.userSurname}</span>
+      </div>
     </li>
   );
 });
+
+
